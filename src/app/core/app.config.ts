83ef49db10d87a77614cf16933f38ef3972d7d6e
Attila Batky <Attila_Batky@epam.com>
2026-03-27 12:38:02 +0100

    fix floating labels

--- src/app/core/app.config.ts
+++ src/app/core/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 import { API_CONFIG } from './api/api-config.token';
 import { routes } from './app.routes';
@@ -12,6 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideHttpClient(),
     provideRouter(routes),
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { floatLabel: 'always' } },
     {
       provide: API_CONFIG,
       useValue: {
